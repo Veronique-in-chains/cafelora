d8be26fd14c4c15fb1b1246efaa46d9740042ab5
Veronika Bosáková <veronikabosakova@gmail.com>
2022-11-29 20:22:33 +0100

    content divided to components

---
+++ src/components/Header/index.js
@@ -0,0 +1,25 @@
+import './style.css';
+
+export const Header = () => {
+  const headerElm = document.createElement('header');
+  headerElm.innerHTML = 
+   `
+    <header>
+    <div class="header__content container">
+      <div class="site-logo"></div>
+
+      <div class="navigation">
+        <button class="nav-btn"></button>
+        <nav class="rollout-nav nav-closed">
+          <a href="#home">domů</a>
+          <a href="#menu">menu</a>
+          <a href="#gallery">galerie</a>
+          <a href="#contact">kontakt</a>
+        </nav>
+      </div>
+
+    </div>
+  </header>
+  `;
+  return headerElm;
+}
