cc41ff70052dac8cdbad24bd4fa59c259e24edde
Veronika Bosáková <veronikabosakova@gmail.com>
2022-12-06 20:13:28 +0100

    Drinks generated from API

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,7 +2,9 @@ import './style.css';
 import { Drink } from './components/Drink/index.js';
 
 
-export const Menu = () => {
+export const Menu = (props = {}) => {
+  const { drinks } = props;
+
   const menuElm = document.createElement('section');
   menuElm.classList = 'menu';
   menuElm.setAttribute('id','menu');
@@ -14,32 +16,28 @@ export const Menu = () => {
     Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
   </p>
   <div class="drinks-list">
-
   </div>
-
   <div class="order-detail">
     <a href="/objednavka">Detail objednávky</a>
   </div>
 </div>
   `;
-  menuElm.querySelector('.drinks-list').append(Drink(
-    {
-      id: 'romano',
-      name: 'Romano',
-      ordered: false,
-      image: 'https://apps.kodim.cz/daweb/cafelora/assets/cups/romano.png',
-      layers: [
-        {
-          color: '#fbdf5b',
-          label: 'citrón',
-        },
-        {
-          color: '#613916',
-          label: 'espresso',
-        },
-      ]
+  const drinksListElm = menuElm.querySelector('.drinks-list');
+
+  if (drinks === undefined) {
+  fetch('https://apps.kodim.cz/daweb/cafelora/api/me/drinks', {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Email veronikabosakova@gmail.com'
     }
-  ));
+  })
+  .then((response) => response.json())
+  .then((data) => drinksListElm.replaceWith(Menu({drinks: data.results}))
+  )} else {
+    const populateDrinks = menuElm.querySelector('.drinks-list');
+    populateDrinks.append(...drinks.map((drink) => Drink(drink)));
+  }
+
   return menuElm;
 }
 
